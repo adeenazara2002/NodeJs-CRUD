32f4daf072cd3af5fb852be2b03a4c040b5deb2d
adeenazara2002 <adinazara2001@gmail.com>
2024-10-22 09:25:52 +0500

    JSON Placeholder

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require("express");
-
+const posts = require("./data/posts"); 
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => console.log(`Server started at port ${PORT}`)
-);
+app.use(express.json()); 
+
+// Routes
+// GET
+
+app.get("/posts", (req, res) => {
+  return res.json(posts); 
+});
+
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
